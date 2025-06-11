86963441140cbd122dc01ba4aa3583271103a575
Yonan Julca <yonanjulca923@gmail.com>
2025-06-11 11:40:27 -0500

    Subiendo proyecto de vidrios

--- src/adapters/primary/http/routes/usuarios_routes.py
+++ src/adapters/primary/http/routes/usuarios_routes.py
@@ -3,4 +3,4 @@ from src.adapters.primary.http.controllers.user_controller import user_blueprint
 
 # Registrar el blueprint de Usuarios 
 usuarios_routes = Blueprint('usuarios_routes', __name__)
-usuarios_routes.register_blueprint(user_blueprint, url_prefix='/usuarios') 
+usuarios_routes.register_blueprint(user_blueprint, url_prefix='/usuarios')
